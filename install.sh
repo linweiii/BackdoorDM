f19430242a4f72a873ac12432d67e6ddf620bd94
Zhou Nanjun <arbiter0102@gmail.com>
2025-03-01 22:20:52 +0800

    update diffusers

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 conda env create -f environment.yml
 conda activate backdoordm
-git clone https://github.com/FrankCCCCC/diffusers.git
+git clone https://github.com/quantum-bitss/diffusers.git
 cd diffusers
 pip install .
 cd ..
